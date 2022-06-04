aeb97d552b47b4009066c5f7ded1785d60054a07
shaq31415926 <sarah.haq@artsymail.com>
2022-06-04 13:44:28 +0200

    updated lecture 3 rock paper game code

--- 03Lecture/rock_paper_scissors.py
+++ 03Lecture/rock_paper_scissors.py
@@ -6,34 +6,45 @@
 # Now that you have the rules down, you can start thinking about how they might translate to Python code.
 # --------
 
-# Take the user input - two users enters their choice
-player_1_selection = input("Player 1 - Enter a choice (rock, paper, scissors): ")
-player_1_selection  = player_1_selection.lower() # lowercase the input
+# This version of the code, asks if the player wants to play again only if they draw
+while True:
+    # Take the user input - two users enters their choice
+    player_1_selection = input("Player 1 - Enter a choice (rock, paper, scissors): ")
+    player_1_selection  = player_1_selection.lower() # lowercase the input
 
-player_2_selection = input("Player 2 - Enter a choice (rock, paper, scissors): ")
-player_2_selection  = player_2_selection.lower() # lowercase the input
+    player_2_selection = input("Player 2 - Enter a choice (rock, paper, scissors): ")
+    player_2_selection  = player_2_selection.lower() # lowercase the input
 
-## do not do next part without testing the first part
+    ## do not do next part without testing the first part
 
-# v1 of the if else statement
-if player_1_selection == "rock":
-    if player_2_selection == "rock":
+    # if the players drawer
+    if  player_1_selection == player_2_selection:
         print("The players draw")
-    elif player_2_selection == "paper":
-        print("The second player wins")
+        play_again = input("Would you like to play again? Y/N:")
+        if play_again.lower() != "y":
+            print("Thank you for playing!")
+            break
+    # otherwise, depending on player selection define which player wins or loses
     else:
-        print("The first player wins")
-elif player_1_selection == "paper":
-    if player_2_selection == "paper":
-        print("The players draw")
-    elif player_2_selection == "scissors":
-        print("The second player wins")
-    else:
-        print("The first player wins")
-elif player_1_selection == "scissors":
-    if player_2_selection == "scissors":
-        print("The players draw")
-    elif player_2_selection == "rock":
-        print("The second player wins")
-    else:
-        print("The first player wins")
+        if player_1_selection == "rock":
+            if player_2_selection == "rock":
+                print("The players draw")
+            elif player_2_selection == "paper":
+                print("The second player wins")
+            else:
+                print("The first player wins")
+        elif player_1_selection == "paper":
+            if player_2_selection == "paper":
+                print("The players draw")
+            elif player_2_selection == "scissors":
+                print("The second player wins")
+            else:
+                print("The first player wins")
+        elif player_1_selection == "scissors":
+            if player_2_selection == "scissors":
+                print("The players draw")
+            elif player_2_selection == "rock":
+                print("The second player wins")
+            else:
+                print("The first player wins")
+        break
