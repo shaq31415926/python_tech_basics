d103b81c3122c60d82be9d664b2689ca20b21b37
shaq31415926 <sarah.haq@artsymail.com>
2022-06-05 22:57:15 +0200

    EH super cool code

--- 03Lecture/rock_paper_scissors_EH.py
+++ 03Lecture/rock_paper_scissors_EH.py
@@ -1,52 +1,44 @@
 # Super cool code by Eldrik that errors if you enter an incorrect option
 
 
-player_1_selection = input("Player 1 - Enter a choice (rock, paper,
-scissors):")
+player_1_selection = input("Player 1 - Enter a choice (rock, paper, scissors): ")
 player_1_selection = player_1_selection.lower() #lowercase the input
 
 
-
-
-while player_1_selection != "rock" and player_1_selection != "paper" and
-player_1_selection != "scissors":
+while player_1_selection != "rock" and player_1_selection != "paper" and player_1_selection != "scissors":
    print("non valid input")
    player_1_selection = input("PLEASE ENTER VALID OPTION:")
 
 
 
-player_2_selection = input("Player 2 - Enter a choice (rock, paper,
-scissors):")
+player_2_selection = input("Player 2 - Enter a choice (rock, paper, scissors):")
 player_2_selection = player_2_selection.lower() #lowercase the input
 
-while player_2_selection != "rock" and player_2_selection != "paper" and
-player_2_selection != "scissors":
+while player_2_selection != "rock" and player_2_selection != "paper" and player_2_selection != "scissors":
    print("non valid input")
    player_2_selection = input("PLEASE ENTER VALID OPTION:")
 
 
-
-
 if player_1_selection == "rock":
    if player_2_selection == "rock":
-   print("Draw")
+       print("Draw")
    elif player_2_selection == "paper":
-   print("Player 2 wins")
+       print("Player 2 wins")
    else:
-   print("Player 1 wins")
+       print("Player 1 wins")
 
 if player_1_selection == "paper":
    if player_2_selection == "paper":
-   print("Draw")
+       print("Draw")
    elif player_2_selection == "scissor":
-   print("Player 2 wins")
+       print("Player 2 wins")
    else:
-   print("Player 1 wins")
+       print("Player 1 wins")
 
 if player_1_selection == "scissor":
    if player_2_selection == "scissor":
-   print("Draw")
+       print("Draw")
    elif player_2_selection == "rock":
-   print("Player 2 wins")
+       print("Player 2 wins")
    else:
-   print("Player 1 wins")
+       print("Player 1 wins")
