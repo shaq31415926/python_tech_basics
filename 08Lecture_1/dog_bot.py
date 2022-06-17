7cdeb134d61e27b4de0fb5bb5fae24e46b8c5bec
shaq31415926 <sarah.haq@artsymail.com>
2022-06-18 01:25:10 +0200

    chatbit code

---
+++ 08Lecture_1/dog_bot.py
@@ -0,0 +1,30 @@
+from telegram import *
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+
+YOUR_TOKEN = "2021611058:AAFJkhuzdFOw5JYCZ26pvjATe2sKxD_fG6o"
+
+def get_dog_image():
+    """Access the API and get the image URL"""
+    contents = requests.get('https://random.dog/woof.json').json()
+    dog_image = contents['url']
+    return dog_image
+
+
+def start(update, context):
+    """The command the bot carries out when starting the bot"""
+    print("all working!")
+    dog_image = get_dog_image()
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=dog_image)
+
+
+def main():
+    updater = Updater(YOUR_TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    updater.start_polling()
+     updater.idle()
+
+
+if __name__ == '__main__':
+    main()
