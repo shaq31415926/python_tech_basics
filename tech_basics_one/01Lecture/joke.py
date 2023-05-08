0f01827b5a4902fd705cf5f8da2e10d6f33b77f8
shaq31415926 <sarah.haq11@gmail.com>
2023-05-08 13:10:26 +0200

    lecture one code

---
+++ tech_basics_one/01Lecture/joke.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+os.system("clear")
+print("-"*50)
+print("HI! This is my joke")
+print("Question: What is Black and White and Red All Over?")
+# time sleep adds a time delay
+time.sleep(4.3)
+print("Answer: A Newspaper.")
+print("Bah Dum Tss")
+print("-"*50)
---
+++ tech_basics_one/01Lecture/joke_1.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+os.system('clear')
+
+print("Question: What is Black and White and Red All Over?")
+print("-")
+time.sleep(0.4)
+print("-")
+time.sleep(0.4)
+print("-")
+time.sleep(0.4)
+print("-")
+time.sleep(0.4)
+print("Answer: A Newspaper.")
+print("Bah Dum Tss")
