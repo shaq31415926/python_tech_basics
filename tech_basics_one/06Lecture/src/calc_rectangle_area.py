3b21964472990a29b03fbddf7290adabea0691ae
shaq31415926 <sarah.haq11@gmail.com>
2023-06-08 16:36:26 +0200

    code from stream b

--- tech_basics_one/06Lecture/src/calc_rectangle_area.py
+++ tech_basics_one/06Lecture/src/calc_rectangle_area.py
@@ -1,6 +1,8 @@
 # Definition to calculate area
 import os
-os.system("clear") # code to clear the terminal for mac users
+
+os.system("clear")  # code to clear the terminal for mac users
+
 
 def calc_rectangle_area(height, width):
     """This definition calculates the area of a rectangle"""
@@ -8,6 +10,7 @@ def calc_rectangle_area(height, width):
     area = height * width
 
     return area
+
 
 height = int(input("What is the height in cm?:"))  # converts the input to an integer
 width = int(input("What is the width in cm?:"))
