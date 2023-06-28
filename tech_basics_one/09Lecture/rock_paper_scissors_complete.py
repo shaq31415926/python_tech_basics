d944b502af432eca9ec392b481f03e8a39d7dccc
shaq31415926 <sarah.haq11@gmail.com>
2023-06-28 19:05:07 +0200

    refactored code for rock paper scissors gui

--- tech_basics_one/09Lecture/rock_paper_scissors_complete.py
+++ tech_basics_one/09Lecture/rock_paper_scissors_complete.py
@@ -1,15 +1,14 @@
-# import the libraries that we will be working with
 import tkinter as tk
 from PIL import Image, ImageTk
 import random
 
-# create the graphical user interfacce
+# code to create the gui window
 root = tk.Tk()
 
 # give your gui a title
-root.title('Rock Paper Scissor')
+root.title("Rock Paper Scissors")
 
-# set the configuration of GUI window
+# code to configure the size
 root.geometry("650x450")
 
 # Create the frame for the first windows of the game
@@ -24,122 +23,152 @@ Lab = tk.Label(f1, image=pic)
 Lab.pack()
 f1.pack()
 
-# add buttons to your first frame so the user can add their name
-# Create some widgets and place them above the image
+# label to ask user for their name
+name_label = tk.Label(root, text="Please Enter Your Name", font="arial 15 bold")
+name_label.place(x=230, y=20)
 
-# print a message asking the user for their name
-name = tk.Label(root, text='Please Enter Your Name :', font='arial 15 bold')
-# where would you like to place this button
-name.place(x=230, y=20)
-# This variable will store the name of user
-nameinp = tk.StringVar()
+# create a box where the user can enter their name
+name = tk.StringVar()  # variable to store the name
+name_box = tk.Entry(root,
+                    textvar=name,
+                    font="arial 15 italic",
+                    fg="blue",
+                    bg="pink")
+name_box.place(x=230, y=60)
 
-# create a message variable which will print the final message
+# create some variables to initialise our labels
 message = tk.Label()
-comp_selection = tk.Label()
-L2 = tk.Label()
-L3 = tk.Label()
-# keep track of scores
-pcscore = 0
-userscore = 0
+comp_select_label = tk.Label()
 
+# create some variables to keep track of the score
+user_score = 0
+comp_score = 0
 
 def computer_selection():
-    # the computer selects rock or paper or scissor at random
-    comp_selection = random.choice(['Rock', 'Paper', 'Scissors'])
+    """This function makes a random selection"""
+    options = ["ROCK", "PAPER", "SCISSORS"]
+    computer_selection = random.choice(options)
 
-    return comp_selection
+    return computer_selection
 
 
-def press(value):
-    global message, comp_selection, pcscore, userscore, nameinp, L2, L3
+def press(user_selects):
+    """This function defines what happens when the user selects an option"""
+    global message, comp_select_label, comp_score, user_score, name
 
-    message.grid_forget()  # We are forgetting or removing old label so that new text can come properly
-    comp_selection.destroy()
-    L2.grid_forget()
-    L3.grid_forget()
+    # clear the labels each time we click on the button
+    message.destroy()
+    comp_select_label.destroy()
 
-    # player_selection = tk.Label(text=f'You have selected {value}', font='arial 15', bg='Black', fg='White')
-    # player_selection.grid(row=8, column=0, pady=15)
+    comp_select = computer_selection()
+    comp_select_label = tk.Label(text=f"The Computer Selects {comp_select}")
+    comp_select_label.grid(row=9, column=0, pady=30)
 
-    computer = computer_selection()
-
-    comp_selection = tk.Label(text=f'The computer selects {computer}', font='arial 15 bold', bg='Red', fg='Black')
-    comp_selection.grid(row=9, column=0, pady=15)
-
-    if value == computer:
-        message = tk.Label(text=f'PLAYERS DRAW! PLEASE PLAY AGAIN', font='arial 15 bold', bg='Black', fg='Yellow')
-    elif value == "Rock" and computer == "Scissors":
-        message = tk.Label(text=f'{nameinp.get().upper()} WINS !! ROCK SMASHES PAPER', font='arial 15 bold', bg='Black', fg='Yellow')
-        userscore += 1
-    elif value == "Scissors" and computer == "Paper":
-        message = tk.Label(text=f'{nameinp.get().upper()} WINS!! SCISSORS CUTS PAPER', font='arial 15 bold', bg='Black', fg='Yellow')
-        userscore += 1
-    elif value == "Paper" and computer == "Rock":
-        message = tk.Label(text=f'{nameinp.get().upper()} ONE WINS!! PAPER COVER ROCKS', font='arial 15 bold', bg='Black', fg='Yellow')
-        userscore += 1
+    if user_selects == comp_select:
+        message = tk.Label(text=f"The computer and {name.get()}  draw. Play again!!!")
+    elif user_selects == "ROCK" and comp_select == "SCISSORS":
+        message = tk.Label(text=f"ROCK SMASHES SCISSORS!!! {name.get().upper()} WINS")
+        user_score += 1
+        # user_score = user_score + 1
+    elif user_selects == "SCISSORS" and comp_select == "PAPER":
+        message = tk.Label(text=f"SCISSORS CUTS PAPER!!! {name.get().upper()} WINS")
+        user_score += 1
+    elif user_selects == "PAPER" and comp_select == "ROCK":
+        message = tk.Label(text=f"PAPER COVER ROCK!!! {name.get().upper()} WINS")
+        user_score += 1
     else:
-        message = tk.Label(text='THE COMPUTER WINS', font='arial 15 bold', bg='Black', fg='Yellow')
-        pcscore += 1
+        message = tk.Label(text="COMPUTER WINS")
+        comp_score += 1
 
-    message.grid(row=10, column=0, pady=30)
+    message.grid(row=10, column=0, pady=15)
 
-    # add a score
-    L2 = tk.Label(text=f"Player Score: {userscore}", bg='#4834DF', fg='#ffffff', borderwidth=5, relief=tk.RAISED,
-               font='Rockwell 13 bold', padx=4, pady=2)
-    L2.grid(row=13, column=0, pady=15)
-    L3 = tk.Label(text=f"PC Score: {pcscore}", bg='#4834DF', fg='white', borderwidth=5, relief=tk.RAISED,
-               font='Rockwell 13 bold', padx=4, pady=2)
-    L3.grid(row=14, column=0, pady=15)
+    # add labels to show the score
+    user_score_label = tk.Label(text=f"Player Score: {user_score}",
+                                relief=tk.RAISED,
+                                bg='#4834DF',
+                                fg='#ffffff',
+                                borderwidth=5
+                                )
+    user_score_label.grid(row=13, column=0, pady=15)
 
+    comp_score_label = tk.Label(text=f"Computer Score: {comp_score}",
+                                relief=tk.RAISED,
+                                bg='#4834DF',
+                                fg='#ffffff',
+                                borderwidth=5
+                                )
+    comp_score_label.grid(row=14, column=0)
+
+
+# create a maingame def that will just clear everything
+def maingame():
+    # create a new gui
+    root.geometry('650x450')
+
+    # destroy all the variables we created for the first window
+    name_label.destroy()
+    f1.destroy()
+    name_box.destroy()
+    play_button.destroy()
+
+    # add the code which creates the layout
+    # designing the layout of our second page
+    welcome = tk.Label(text="Welcome to Rock vs Paper vs Scissors",
+                       font="arial 20 bold",
+                       bg="Pink",
+                       fg="Blue"
+                       )
+    welcome.grid(row=0, columnspan=1, ipadx=70, padx=33, pady=10)
+
+    second_message = tk.Label(text=f"Good luck {name.get().capitalize()}!! Please make your selection",
+                              font="arial 15 bold",
+                              bg="White",
+                              fg="Blue"
+                              )
+    second_message.grid(row=1, column=0, pady=15)
+
+    # add some buttons the player can select
+    rock = tk.Button(text="ROCK",
+                     font="arial 14 bold",
+                     height=1,
+                     width=7,
+                     command=lambda: press("ROCK")
+                     )
+    rock.grid(row=3, column=0)
+
+    paper = tk.Button(text="PAPER",
+                      font="arial 14 bold",
+                      height=1,
+                      width=7,
+                      command=lambda: press("PAPER")
+                      )
+    paper.grid(row=4, column=0)
+
+    scissors = tk.Button(text="SCISSOR",
+                         font="arial 14 bold",
+                         height=1,
+                         width=7,
+                         command=lambda: press("SCISSOR")
+                         )
+    scissors.grid(row=5, column=0)
+
+    # THE FINAL BUTTON WHICH CLOSES THE GAME
+    exit_button = tk.Button(text="Close the Game",
+                            command=root.destroy
+                            )
+    exit_button.place(x=500, y=410)
+
+
+# add another box, that will enter the game when the user clicks on it
+play_button = tk.Button(text="Let's Play", font='lucida 10 bold', command=maingame)
+play_button.place(x=305, y=88)
 
 def entergame(event):
     maingame()
 
-def maingame():
-    global nameinp
-
-    # create a new gui
-    root.geometry('650x450')
-    # destroy all the variables we created for the first window
-    name.destroy()
-    f1.destroy()
-    inpname.destroy()
-    play_button.destroy()
-
-    # Layout of RPS Game
-    # add buttons for player 1 and player 2
-
-    head = tk.Label(text='Welcome to Rock vs Paper vs Scissors', font='arial 25 bold', bg='Pink', fg='Black')
-    # place on a grid
-    head.grid(columnspan=1, row=0, ipadx=70, padx=33, pady=10)
-    # add a label for player one
-    player_one = tk.Label(text=f'Good Luck {nameinp.get().capitalize()}!!! Make your selection', font='Arial 16', fg='white')
-    player_one.grid(row=1, column=0, pady=15)
-
-    # add buttons the player can select
-    rock = tk.Button(text='Rock', font='arial 14 bold', height=1, width=7, command=lambda: press("Rock"))
-    rock.grid(row=3, column=0)
-    paper = tk.Button(text='Paper', font='arial 14 bold', height=1, width=7, command=lambda: press("Paper"))
-    paper.grid(row=4, column=0)
-    scissors = tk.Button(text='Scissors', font='arial 14 bold', height=1, width=7, command=lambda: press("Scissors"))
-    scissors.grid(row=5, column=0)
-
-    # add a close button which will close the game
-    btnclose = tk.Button(text='Close Game', command=root.destroy, font='arial 10 bold')
-    btnclose.place(x=500, y=410)
-
-
-# create an entry box to store the users name
-inpname = tk.Entry(root, textvar=nameinp, font='arial 15 bold')
-# We binded Return event with inpname entry widget i.e. if enter key is pressed then entergame function will be called
-inpname.bind('<Return>',
+# add code that will enter the game when you press enter
+name_box.bind('<Return>',
              entergame)
-inpname.place(x=230, y=60)
 
-# add a let's play button
-
-play_button = tk.Button(text="Let's Play", font='lucida 10 bold', command=maingame)
-play_button.place(x=305, y=88)
-
+# code to execute the code
 root.mainloop()
