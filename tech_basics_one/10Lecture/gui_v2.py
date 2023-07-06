9d16583e502c4ec1ae1307d99e2abaf3d6210f62
shaq31415926 <sarah.haq11@gmail.com>
2023-07-06 17:38:52 +0200

    gui with back button

--- tech_basics_one/10Lecture/gui_v2.py
+++ tech_basics_one/10Lecture/gui_v2.py
@@ -35,16 +35,13 @@ def add_image(root, file_path):
     f1.pack()
 
 
-# call our add_image function with the root variable image file path
-add_image(root, file_path="images/store.jpeg")
-# you should see the image appearing in the first screen
-
-def maingame():
+def page_two():
     """This definition does the following:
     1. Destroys everything we created in the first window
     2. Adds a new image to the background
     3. Creates a new label
     """
+    global back_button, test_label
 
     # destroy all objects we created in first image
     f1.destroy()
@@ -63,14 +60,39 @@ def maingame():
     # if you place a background image, grid does not seem to work to put the labels/buttons
     test_label.place(x=10, y=10)
 
+    # add a button that goes back to the first page
+    back_button = tk.Button(text="Back",
+                             font='lucida 20 bold',
+                             command=back,
+                             fg="blue")
+    # play around with the x and y coordinates to place your button
+    back_button.place(x=500, y=120)
 
-# add a box on the first gui window, that will clear everything and launch a new page
-enter_button = tk.Button(text="Click here to enter store",
-                         font='lucida 20 bold',
-                         command=maingame,
-                         fg="blue")
-# play around with the x and y coordinates to place your button
-enter_button.place(x=200, y=120)
+def page_one():
+    global enter_button
+
+    # call our add_image function with the root variable image file path
+    add_image(root, file_path="images/store.jpeg")
+    # you should see the image appearing in the first screen
+
+    # add a box on the first gui window, that will clear everything and launch a new page
+    enter_button = tk.Button(text="Click here to enter store",
+                             font='lucida 20 bold',
+                             command=page_two,
+                             fg="blue")
+    # play around with the x and y coordinates to place your button
+    enter_button.place(x=200, y=120)
+
+def back():
+    # destroy all objects we created in second image
+    f1.destroy()
+    back_button.destroy()
+    test_label.destroy()
+
+    page_one()
+
+# call our first page function which adds an image and an enter button
+page_one()
 
 # code to execute the code
 root.mainloop()
