1180c6f18127be8d2290b6ccc20f9548b454f5d7
shaq31415926 <sarah.haq11@gmail.com>
2024-01-15 22:05:37 +0100

    simple chatbot code

---
+++ tech_basics_two/11Lecture/simple_chatbot.py
@@ -0,0 +1,66 @@
+# Reference: The code was inspired by https://www.geeksforgeeks.org/gui-chat-application-using-tkinter-in-python/
+import tkinter as tk
+
+# create the gui
+root = tk.Tk()
+root.title("ChatBot")
+# size the size
+screen_width = 600
+screen_height = 400
+
+# option 1: use the minsize function
+root.minsize(screen_width, screen_height)
+
+# create variables to format
+bg_colour = "#17202A"
+text_colour = "#EAECEE"
+font = "Helvetica 14"
+font_bold = "Helvetica 13 bold"
+
+# place a welcome label
+welcome_label = tk.Label(root,
+                         text="Welcome to the ChatBot",
+                         bg=bg_colour,
+                         fg=text_colour,
+                         font=font_bold,
+                         # pady=10,
+                         width=30,
+                         height=1)
+welcome_label.place(relx=0.5,
+                    y=17,
+                    anchor=tk.CENTER)  # place in center
+
+# create the text box
+text_box = tk.Text(root,
+                   bg=bg_colour,
+                   fg=text_colour,
+                   font=font,
+                   width=60)
+text_box.place(x=0, y=30, relwidth=1, relheight=1)  # these attributes ensure it takes up the entire screen
+# text_box.insert(tk.END, "\n" + "Bot -> Hi there, what can I do for you?")
+
+# create a scrollbar for the textbox
+scroll_bar = tk.Scrollbar(text_box)
+scroll_bar.place(relheight=1,
+                 relx=0.974)
+
+# create an entry box for user to enter message
+entry_box = tk.Entry(root,
+                     bg="#2C3E50",
+                     fg=text_colour,
+                     font=font,
+                     width=55
+                     )
+entry_box.place(x=5,
+                rely=0.925)
+
+send_button = tk.Button(root,
+                        text="Send",
+                        font=font_bold,
+                        bg="#ABB2B9",
+                        width=10)
+send_button.place(relx=0.77,
+                  rely=0.925
+                      )
+
+root.mainloop()
