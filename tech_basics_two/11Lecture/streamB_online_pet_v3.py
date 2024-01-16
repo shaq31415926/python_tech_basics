3e2905b57d7af06e4b1c342d04467f23c103ab9b
shaq31415926 <sarah.haq11@gmail.com>
2024-01-16 15:53:10 +0100

    live coding

--- tech_basics_two/11Lecture/streamB_online_pet_v3.py
+++ tech_basics_two/11Lecture/streamB_online_pet_v3.py
@@ -10,6 +10,72 @@ screen_width = 600
 screen_height = 400
 root.minsize(screen_width, screen_height)
 
+# create a homepage button definition
+def create_homepage_button():
+   homepage = tk.Button(root,
+                        text="🏠",
+                        command=create_homepage
+                        )
+   homepage.pack(side=tk.BOTTOM)
+
+
+
+# create your new user page definition
+def create_new_userpage():
+    # clear widgets
+    clear_widgets(root)
+
+    # create a welcome label
+    new_label = tk.Label(root,
+                         text="WELCOME NEW USER!!",
+                         font=("Comic Sans MS", 28, "bold"))
+    new_label.pack(side=tk.TOP, anchor=tk.CENTER)
+
+    # Create labels and entry boxes to get user information
+    # ask for the name of the user
+    name_label = tk.Label(root,
+                          text="What is your name?!",
+                          fg="white"
+                          )
+    name_label.place(x=50, y=100)
+
+    # entry box
+    name = tk.StringVar()
+    name_box = tk.Entry(root,
+                        textvar=name,
+                        fg="white",
+                        bg="blue"
+                        )
+    name_box.place(x=200, y=100)
+
+    # ask for a user name
+    username_label = tk.Label(root,
+                              text="Create a user name",
+                              fg="white"
+                              )
+    username_label.place(x=50, y=150)
+
+    # entry box
+    username = tk.StringVar()
+    username_box = tk.Entry(root,
+                            textvar=username,
+                            fg="white",
+                            bg="blue"
+                            )
+    username_box.place(x=200, y=150)
+
+    # TODO: create a submit information button
+    # create a button to store all the information
+    enter_data = tk.Button(root,
+                           text="SUBMIT INFO",
+                           # command=enter_user_data
+                           )
+    enter_data.place(x=250, y=200)
+
+    # add a homepage button
+    create_homepage_button()
+
+
 # CREATE A DEFINITION THAT WILL CREATE YOUR HOMPAGE
 def create_homepage():
     """This function creates the homepage"""
@@ -34,7 +100,7 @@ def create_homepage():
     new_user = tk.Button(root,
                          text="New User",
                          font=("Comic Sans MS", 14, "bold"),
-                         #command=new_user_page
+                         command=create_new_userpage
                          )
     new_user.pack()
 
