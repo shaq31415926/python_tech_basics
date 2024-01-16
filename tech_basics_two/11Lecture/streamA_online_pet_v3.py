05e316ff6ee92903b8449f01b982a276490646eb
shaq31415926 <sarah.haq11@gmail.com>
2024-01-16 12:59:09 +0100

    live code from class

--- tech_basics_two/11Lecture/streamA_online_pet_v3.py
+++ tech_basics_two/11Lecture/streamA_online_pet_v3.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import pandas as pd
 from src.helpers import add_image, clear_widgets
+
 # from src.simple_chatbot_v2 import create_chatbot_button
 
 # create the gui
@@ -14,9 +15,17 @@ screen_height = 400
 # option 1: use the minsize function
 root.minsize(screen_width, screen_height)
 
+
 # add the add image definition here if needed
 
 # add the clear widgets here if needed
+
+def create_homepage_button():
+    homepage = tk.Button(root,
+                         text="🏠",
+                         command=create_homepage
+                         )
+    homepage.pack(side=tk.BOTTOM)
 
 
 def enter_user_data():
@@ -45,6 +54,9 @@ def enter_user_data():
                               text="Thanks! Your info has been submitted")
     thankyou_label.place(x=50, y=50)
     # I can add more to this page here...
+
+    # place a homepage button here
+    create_homepage_button()
 
 
 def create_new_userpage():
@@ -102,11 +114,55 @@ def create_new_userpage():
     enter_data.place(x=250, y=200)
 
     # add a home page button - this will go back to our home page
-    homepage = tk.Button(root,
-                         text="🏠",
-                         command=create_homepage
-                         )
-    homepage.pack(side=tk.BOTTOM)
+    create_homepage_button()
+
+
+def check_user():
+    # read all the usernames
+    user_ids = list(pd.read_csv("data/user_data.csv").username)
+
+    # check if username exists - then go onto next page
+    if user_id.get() in user_ids:
+        clear_widgets(root)
+        create_homepage_button()
+        # ADD MORE CODE
+   # otherwise give a warning
+    else:
+        tk.messagebox.showwarning("WARNING", "User does not exist")
+
+
+def create_returning_userpage():
+    global user_id
+
+    # clear widgets
+    clear_widgets(root)
+
+    # entry box to ask for the username
+    # ask for a user name
+    userid_label = tk.Label(root,
+                              text="Enter your user name",
+                              fg="white"
+                              )
+    userid_label.place(x=50, y=150)
+
+    # entry box
+    user_id = tk.StringVar()
+    userid_box = tk.Entry(root,
+                            textvar=user_id,
+                            fg="white",
+                            bg="blue"
+                            )
+    userid_box.place(x=200, y=150)
+
+    # add a button to login
+    login_button = tk.Button(root,
+              text="login",
+              command=check_user
+              )
+    login_button.place(x=250, y=200)
+
+    # place a homepage button
+    create_homepage_button()
 
 
 def create_homepage():
@@ -139,12 +195,13 @@ def create_homepage():
     returning_user = tk.Button(root,
                                text="Returning User",
                                font=("Comic Sans MS", 14, "bold"),
-                               command=lambda:clear_widgets(root)
+                               command=create_returning_userpage
                                )
     returning_user.pack()
 
     # if you want to, you can add the create chatbot button here
-    #create_chatbot_button(root)
+    # create_chatbot_button(root)
+
 
 # create the home page
 create_homepage()
